ae0c9756a15dfb22a691c8842f3dcce33f3947d7
Dareck27 <27dareck@gmail.com>
2026-03-21 20:19:15 -0600

    Catalogo Artistas

---
+++ php/vista-index.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once __DIR__ . "/lib/manejaErrores.php";
+require_once __DIR__ . "/lib/devuelveJson.php";
+require_once __DIR__ . "/Bd.php";
+
+//  conexión
+$pdo = Conexion::getInstance()->getConnection();
+
+//  consulta a TU tabla real
+$sql = "SELECT 
+ art_id,
+ art_nombre,
+ art_foto,
+ art_especialidad,
+ art_experiencia,
+ art_disponible
+ FROM artista
+ WHERE art_disponible = 1
+ ORDER BY art_nombre";
+
+$stmt = $pdo->query($sql);
+$lista = $stmt->fetchAll();
+
+$render = "";
+
+foreach ($lista as $a) {
+
+ $id = htmlentities(urlencode($a["art_id"]));
+ $nombre = htmlentities($a["art_nombre"]);
+ $especialidad = htmlentities($a["art_especialidad"]);
+ $experiencia = htmlentities($a["art_experiencia"]);
+ $foto = htmlentities($a["art_foto"]);
+
+ $render .= "
+  <div class='card'>
+   <img src='$foto' alt='$nombre' width='200' height='200'>
+   <h3>$nombre</h3>
+   <p>Especialidad: $especialidad</p>
+   <p>Experiencia: $experiencia años</p>
+   <a href='#?id=$id'>Agendar cita</a>
+  </div>
+ ";
+}
+
+
+devuelveJson([ "lista" => ["innerHTML" => $render]]);
